ded93434701c64d52a39ac68d4f44d06147a5f3e
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-07-24 11:10:08 +0300

    add controller

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    [SerializeField] private Rigidbody2D _rb = null;
+    private Vector2 _moveDirection;
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInput() 
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        _moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move() 
+    {
+        _rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+}
