743af62590ccd50a4a42f42e0a6af027ffbe1bd2
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-07-24 17:47:20 +0300

    add wheapon throw

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,10 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public bool inTimerZone;
-    public bool stunned = false;
     [SerializeField] private SpriteRenderer _renderer = null;
     [SerializeField] private Rigidbody2D _rb = null;
-    [SerializeField] private Text _text = null;
     private Vector2 _moveDirection;
     private bool _inputLocked = false;
-
-    private void Start()
-    {
-        _text.color = Color.cyan;
-        _text.text = "";
-    }
 
     private void Update()
     {
@@ -47,34 +38,5 @@ public class Player : MonoBehaviour
     private void Move() 
     {
         _rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Projectile")
-        {
-            if (inTimerZone && !stunned)
-            {
-                StartCoroutine("Stun");
-            }
-        }
-    }
-
-    private IEnumerator Stun() 
-    {
-        _inputLocked = true;
-        stunned = true;
-        _renderer.color = new Color(_renderer.color.r, _renderer.color.g, _renderer.color.b, 0.5f);
-        _text.color = Color.yellow;
-        _text.text = "Stunned";
-        yield return new WaitForSeconds(2f);
-        _inputLocked = false;
-        _text.color = Color.cyan;
-        _text.text = "Invulnerable";
-        yield return new WaitForSeconds(1f);
-        stunned = false;
-        _renderer.color = new Color(_renderer.color.r, _renderer.color.g, _renderer.color.b, 1f);
-        _text.color = Color.cyan;
-        _text.text = "";
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,10 +11,17 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject _projectile = null;
     private GameObject _projectileInstance = null;
     private Projectile _projectileInstanceScript = null;
+    private bool touchedByPlayer = false;
 
     private void Update()
     {
-        if (_projectileInstance == null) 
+        ProcessInput();
+        Debug.Log(touchedByPlayer);
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && touchedByPlayer)
         {
             _projectileInstance = Instantiate(_projectile, transform);
             _projectileInstanceScript = _projectileInstance.GetComponent<Projectile>();
@@ -23,6 +30,21 @@ public class Spawner : MonoBehaviour
             _projectileInstanceScript.rotationSpeed = rotationSpeed;
             _projectileInstanceScript.rotateClockWise = rotateClockWise;
             _projectileInstanceScript.destroyY = destroyY;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            touchedByPlayer = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            touchedByPlayer = false;
         }
     }
 }
